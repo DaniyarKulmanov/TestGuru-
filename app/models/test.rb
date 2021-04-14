edd923342ab8305fd81de390cd3109ce57999da0
DaniyarKulmanov <DanRubyDeveloper@gmail.com>
2021-04-14 16:59:42 +0600

    Tests by category title

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,3 +1,7 @@
 class Test < ApplicationRecord
   # belongs_to :category
+  def self.titles(category)
+    category_id = Category.find_by(title: category)
+    Test.where(category_id: category_id).pluck(:title).sort.reverse
+  end
 end
