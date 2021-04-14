50a480f589b0735db583ca738402a4a89b81ea48
DaniyarKulmanov <DanRubyDeveloper@gmail.com>
2021-04-14 11:00:07 +0600

    AddCategoryRefToTests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_10_105443) do
+ActiveRecord::Schema.define(version: 2021_04_14_045318) do
 
   create_table "answers", force: :cascade do |t|
     t.boolean "correct", default: false
@@ -40,7 +40,9 @@ ActiveRecord::Schema.define(version: 2021_04_10_105443) do
     t.integer "author_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "category_id", null: false
     t.index ["author_id"], name: "index_tests_on_author_id"
+    t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -51,5 +53,6 @@ ActiveRecord::Schema.define(version: 2021_04_10_105443) do
 
   add_foreign_key "answers", "questions"
   add_foreign_key "questions", "tests"
+  add_foreign_key "tests", "categories"
   add_foreign_key "tests", "users", column: "author_id"
 end
