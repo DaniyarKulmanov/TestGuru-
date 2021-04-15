68c85cba4434b345a777e4f033283046dbfef743
DaniyarKulmanov <DanRubyDeveloper@gmail.com>
2021-04-15 12:04:50 +0600

    clean comments

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,5 +1,4 @@
 class Test < ApplicationRecord
-  # belongs_to :category
   def self.titles(category)
     category_id = Category.find_by(title: category)
     Test.where(category_id: category_id).pluck(:title).sort.reverse
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-  # belongs_to :test
   def test_by_level(level)
     Test.find_by(level: level, author_id: id)
   end
