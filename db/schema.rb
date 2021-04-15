40c3bef28d90a70f9480ae6de4e3e99c4ee59ada
DaniyarKulmanov <DanRubyDeveloper@gmail.com>
2021-04-15 21:26:21 +0600

    result join model/table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_14_045318) do
+ActiveRecord::Schema.define(version: 2021_04_15_151025) do
 
   create_table "answers", force: :cascade do |t|
     t.boolean "correct", default: false
@@ -32,6 +32,15 @@ ActiveRecord::Schema.define(version: 2021_04_14_045318) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["test_id"], name: "index_questions_on_test_id"
+  end
+
+  create_table "results", id: false, force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "test_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["test_id"], name: "index_results_on_test_id"
+    t.index ["user_id"], name: "index_results_on_user_id"
   end
 
   create_table "tests", force: :cascade do |t|
