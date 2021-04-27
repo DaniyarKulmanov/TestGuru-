6cf616bd14b9a2a7877388356deb01bf7a02ee87
DaniyarKulmanov <DanRubyDeveloper@gmail.com>
2021-04-27 15:15:29 +0600

    test.rb validations

--- app/models/test.rb
+++ app/models/test.rb
@@ -11,6 +11,10 @@ class Test < ApplicationRecord
   has_many :results, dependent: :destroy
   has_many :users, through: :results
 
+  validates :title, presence: true
+  validates :level, numericality: { greater_than_or_equal_to: 0 }
+  validates :title, uniqueness: { scope: :level, message: 'Title and level must be uniq' }
+
   scope :beginner, -> { where(level: 0..1) }
   scope :advanced, -> { where(level: 2..4) }
   scope :pro, -> { where(level: 5..INFINITE) }
