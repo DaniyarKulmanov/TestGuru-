2a0fbeb035f19aca193c89fa25ba2347fc723247
DaniyarKulmanov <DanRubyDeveloper@gmail.com>
2021-04-27 14:52:46 +0600

    user.rb validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   has_many :results, dependent: :destroy
   has_many :tests, through: :results
 
+  validates :name, presence: true
+
   scope :test_by_level, lambda { |level, id|
     joins(:tests, :results).where(tests: { level: level }, results: { user_id: id })
   }
