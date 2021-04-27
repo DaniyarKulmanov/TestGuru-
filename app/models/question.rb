5a3a976d3023d829d9874750808c35f3b4609c54
DaniyarKulmanov <DanRubyDeveloper@gmail.com>
2021-04-27 15:35:46 +0600

    questions validations

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,4 +4,13 @@ class Question < ApplicationRecord
   belongs_to :test
 
   has_many :answers, dependent: :destroy
+
+  validates :body, presence: true
+  validate :validate_answers
+
+  private
+
+  def validate_answers
+    errors.add(:answers, 'Must be 1 - 4 answers') unless (1..4).include? answers.size
+  end
 end
