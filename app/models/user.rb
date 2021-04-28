8f8f8db136f14d5fae484237467a53b48f57e84d
DaniyarKulmanov <DanRubyDeveloper@gmail.com>
2021-04-28 18:33:23 +0600

    users, add email and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
   has_many :results, dependent: :destroy
   has_many :tests, through: :results
 
-  validates :name, presence: true
+  validates :name, :email, presence: true
+  validates :email, format: URI::MailTo::EMAIL_REGEXP
 
   scope :test_by_level, lambda { |level, id|
     joins(:tests, :results).where(tests: { level: level }, results: { user_id: id })
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_28_110100) do
+ActiveRecord::Schema.define(version: 2021_04_28_122343) do
 
   create_table "answers", force: :cascade do |t|
     t.boolean "correct", default: false
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 2021_04_28_110100) do
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email"
   end
 
   add_foreign_key "answers", "questions"
