a7b49a93fbffd33bb0209f7b241ef7957a9b9c1f
DaniyarKulmanov <DanRubyDeveloper@gmail.com>
2021-04-28 19:17:50 +0600

    refactor answers 1-4 per question validation

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -5,5 +5,18 @@ class Answer < ApplicationRecord
 
   scope :proper, -> { where(correct: true) }
 
-  validates :correct, :body, presence: true
+  validates :body, presence: true
+  validates :correct, inclusion: { in: [true, false] }
+  validate :validate_answers_count, if: :question_exist?
+
+  private
+
+  def validate_answers_count
+    errors.add(:question, 'only 1 - 4 question allowed') unless (1..4).include?(question.answers.size)
+    puts (1..4).include?(question.answers.size)
+  end
+
+  def question_exist?
+    !question.nil?
+  end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -6,11 +6,4 @@ class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
 
   validates :body, presence: true
-  # validate :validate_answers
-
-  private
-
-  def validate_answers
-    errors.add(:answers, 'Must be 1 - 4 answers') unless (1..4).include? answers.size
-  end
 end
