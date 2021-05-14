138c20bab1f9c5db6a953a8cddd3d786c0db43ea
DaniyarKulmanov <DanRubyDeveloper@gmail.com>
2021-05-14 11:53:30 +0600

    delete tests & answers controllers

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,20 +1,29 @@
 # frozen_string_literal: true
 
 class QuestionsController < ApplicationController
-  before_action :find_test, only: :create
+  before_action :set_question, only: %i[show create]
+  rescue_from ::ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
+
+
+  def show; end
 
   def create
     @question = @test.questions.create(question_params)
-    redirect_to test_path(@test)
+    # redirect_to test_path(@test)
+    render html: '<h1>Question created</h1>'.html_safe
   end
 
   private
 
-  def question_params
-    params.require(:question).permit(:body)
+  def set_question
+    @question = Question.find(params[:id])
   end
 
-  def find_test
-    @test = Test.find(params[:test_id])
+  def rescue_with_question_not_found
+    render plain: 'Question not found'
+  end
+
+  def question_params
+    params.require(:question).permit(:body, :test_id)
   end
 end
