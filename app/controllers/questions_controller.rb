d74297292995a20710c6ce339e77a17ee00d0064
DaniyarKulmanov <DanRubyDeveloper@gmail.com>
2021-05-14 12:15:37 +0600

    all questions / single question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,19 +1,29 @@
 # frozen_string_literal: true
 
 class QuestionsController < ApplicationController
+  before_action :set_test
   before_action :set_question, only: %i[show create]
   rescue_from ::ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
+  def index
+    @questions = Question.where(test_id: @test)
+    render inline: 'All Questions: <%= @questions.each{ |question| puts question  }%>'
+  end
 
-  def show; end
+  def show
+    render inline: 'Question: <%= @question.inspect %>'
+  end
 
   def create
     @question = @test.questions.create(question_params)
     # redirect_to test_path(@test)
-    render html: '<h1>Question created</h1>'.html_safe
   end
 
   private
+
+  def set_test
+    @test = Test.find(params[:test_id])
+  end
 
   def set_question
     @question = Question.find(params[:id])
