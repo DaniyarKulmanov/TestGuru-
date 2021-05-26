def8344ca5dfca4fd8b8dc258b0fefe4fabaecc0
DaniyarKulmanov <DanRubyDeveloper@gmail.com>
2021-05-26 15:14:33 +0600

    question header helper for new/edit

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,9 @@
+# frozen_string_literal: true
+
 module QuestionsHelper
+  ACTION_HEADER = { new: 'Create New', edit: 'Edit' }.freeze
+
+  def question_header
+    "#{ACTION_HEADER[params[:action].to_sym]} #{@question.test.title} Question"
+  end
 end
