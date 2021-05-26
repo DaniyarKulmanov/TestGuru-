f13f8cdfc37a5d5269f8fef6805cd422c357b7f1
DaniyarKulmanov <DanRubyDeveloper@gmail.com>
2021-05-26 15:55:46 +0600

    footer 2nd

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,14 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  AUTHOR = 'DaniyarKulmanov'
+  PROJECT = 'TestGuru-'
+
   def current_year
     Date.today.year
   end
 
-  def github_url(author, repo)
+  def github_url(author, repo = nil)
     "https://github.com/#{author}/#{repo}"
   end
 end
