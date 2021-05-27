92e996b71eb029f7292853ab472928892931bf72
DaniyarKulmanov <DanRubyDeveloper@gmail.com>
2021-05-27 10:58:46 +0600

    index, show, new, create actions and views

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,11 +1,30 @@
 # frozen_string_literal: true
 
 class TestsController < ApplicationController
+  before_action :set_test, only: [:show]
+
   def index
     @tests = Test.all
   end
 
-  def show
+  def show; end
+
+  def new
+    @test = Test.new
+  end
+
+  def create
+    @test = Test.new(test_params)
+    @test.save ? (redirect_to @test) : (render :new)
+  end
+
+  private
+
+  def test_params
+    params.require(:test).permit(:title, :level, :category_id, :author_id)
+  end
+
+  def set_test
     @test = Test.find(params[:id])
   end
 end
