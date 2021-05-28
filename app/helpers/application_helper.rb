754eebfe5b5becec28758eef5ecad54ba7a8782d
DaniyarKulmanov <DanRubyDeveloper@gmail.com>
2021-05-28 13:40:32 +0600

    refactor github_url helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,6 +6,7 @@ module ApplicationHelper
   end
 
   def github_url(author, repo = nil)
-    "https://github.com/#{author}/#{repo}"
+    title = repo.nil? ? "Автор #{author}" : 'Проект'
+    "<a href='https://github.com/#{author}/#{repo}' target='_blank'>#{title} #{repo}</a>".html_safe
   end
 end
