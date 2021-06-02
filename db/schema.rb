a6fd87cfdc2d298087f27d057dcea46788be9de7
DaniyarKulmanov <DanRubyDeveloper@gmail.com>
2021-06-02 22:04:42 +0600

    Add current_question_id to results

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_29_075044) do
+ActiveRecord::Schema.define(version: 2021_06_02_155025) do
 
   create_table "answers", force: :cascade do |t|
     t.boolean "correct", default: false, null: false
@@ -40,6 +40,8 @@ ActiveRecord::Schema.define(version: 2021_04_29_075044) do
     t.integer "test_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "current_question_id"
+    t.index ["current_question_id"], name: "index_results_on_current_question_id"
     t.index ["test_id"], name: "index_results_on_test_id"
     t.index ["user_id"], name: "index_results_on_user_id"
   end
@@ -64,6 +66,7 @@ ActiveRecord::Schema.define(version: 2021_04_29_075044) do
 
   add_foreign_key "answers", "questions"
   add_foreign_key "questions", "tests"
+  add_foreign_key "results", "questions", column: "current_question_id"
   add_foreign_key "tests", "categories"
   add_foreign_key "tests", "users", column: "author_id"
 end
