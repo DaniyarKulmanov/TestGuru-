8f7e691177901763664763a9ac8d471e02ca4be1
DaniyarKulmanov <DanRubyDeveloper@gmail.com>
2021-06-09 21:40:06 +0600

    color result

--- app/helpers/results_helper.rb
+++ app/helpers/results_helper.rb
@@ -1,2 +1,8 @@
+# frozen_string_literal: true
+
 module ResultsHelper
+  def attempt_information
+    result = @result.count_passage_result
+    @result.passed? ? "<p span class='positive'>#{result}</p>".html_safe : "<p span class='negative'>#{result}</p>".html_safe
+  end
 end
