cdff9619a9081f1f63a8e2165aacc57ecfaa70a1
DaniyarKulmanov <DanRubyDeveloper@gmail.com>
2021-06-17 11:49:45 +0600

    delete obsolete helper for tests

--- app/helpers/tests_helper.rb
+++ app/helpers/tests_helper.rb
@@ -1,11 +1,4 @@
 # frozen_string_literal: true
 
 module TestsHelper
-  def start_button(test)
-    if test.questions.empty?
-      'No questions found for test'
-    else
-      button_to 'start', begin_test_path(test)
-    end
-  end
 end
