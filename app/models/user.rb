d34252bf3cc2a4135768a235895bd2559cada74b
DaniyarKulmanov <DanRubyDeveloper@gmail.com>
2021-06-22 17:01:36 +0600

    has_secure_password and gem bcrypt ~> 3.1.7

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,14 @@
 
 class User < ApplicationRecord
 
-  include Auth
-
   has_many :results, dependent: :destroy
   has_many :tests, through: :results
   has_many :created_tests, class_name: 'Test', foreign_key: :author_id
+
+  has_secure_password
+
+  validates :name, :email, presence: true
+  validates :email, format: URI::MailTo::EMAIL_REGEXP
 
   def test_by_level(level)
     Test.joins(:results).where(level: level, results: { user_id: id })
