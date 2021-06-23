a37a886a07ed8f3a2fa543dc3f83e1c2ea3b30cd
DaniyarKulmanov <DanRubyDeveloper@gmail.com>
2021-06-23 10:09:48 +0600

    authenticate_user in application rb & controllers tests, questions, answers

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class TestsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_test, only: %i[show edit update destroy begin]
 
   def index
