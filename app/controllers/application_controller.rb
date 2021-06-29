3d137db085031c1853d1cdc33dacc753e8bd9fd0
DaniyarKulmanov <DanRubyDeveloper@gmail.com>
2021-06-29 19:16:19 +0600

    redirect admins to admins/tests with devise helper after_sign_in_path_for

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  def after_sign_in_path_for(resource)
+    if resource.is_a?(Admin)
+      admin_tests_path
+    else
+      super
+    end
+  end
 end
