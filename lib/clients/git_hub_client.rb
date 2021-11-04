a09ea82b134ee4c3a42a2f74d7a010f55e1ec660
DaniyarKulmanov <DanRubyDeveloper@gmail.com>
2021-11-04 19:15:55 +0600

    gem dotenv-rails

--- lib/clients/git_hub_client.rb
+++ lib/clients/git_hub_client.rb
@@ -1,12 +1,11 @@
 class GitHubClient
-  # ACCESS_TOKEN = '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'
 
   def initialize
-    @client = Octokit::Client.new(access_token: ACCESS_TOKEN)
+    @client = Octokit::Client.new(access_token: ENV['ACCESS_TOKEN'])
   end
 
   def create_gist(params)
-    @client.create_gist(params)
+    gist = @client.create_gist(params)
   end
 
 end
