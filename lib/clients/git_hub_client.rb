c715a1c871f85ffe8cec4664ce47aeae727a0d67
DaniyarKulmanov <DanRubyDeveloper@gmail.com>
2021-11-04 16:33:29 +0600

    token local

--- lib/clients/git_hub_client.rb
+++ lib/clients/git_hub_client.rb
@@ -1,5 +1,5 @@
 class GitHubClient
-  ACCESS_TOKEN = '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'
+  # ACCESS_TOKEN = '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'
 
   def initialize
     @client = Octokit::Client.new(access_token: ACCESS_TOKEN)
