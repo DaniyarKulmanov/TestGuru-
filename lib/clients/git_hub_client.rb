c43ad47bf67bc1f28a37b0dcc4297203fcd5b34f
DaniyarKulmanov <DanRubyDeveloper@gmail.com>
2021-11-08 10:52:57 +0600

    re-created model gist, saving gist to db

--- lib/clients/git_hub_client.rb
+++ lib/clients/git_hub_client.rb
@@ -1,20 +1,23 @@
-class GitHubClient
+# frozen_string_literal: true
 
+class GitHubClient
   def initialize
     @client = Octokit::Client.new(access_token: ENV['ACCESS_TOKEN'])
   end
 
   def publish_gist(gist_params, question, user)
-    gist = @client.create_gist( gist_params )
-    # register_gist(gist, question, user)
+    gist = @client.create_gist(gist_params)
+    registegr_gist(gist, question, user)
   end
 
   private
 
   def register_gist(gist, question, user)
-    # Gist.create(
-    #   question: question,
-    #   user: user,
-    #   url: gist[:html_url])
+    Gist.create(
+      html_url: gist[:html_url],
+      guid: gist[:id],
+      user: user,
+      question: question
+    )
   end
 end
