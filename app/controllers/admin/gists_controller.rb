e32f26fec459107b04624ec3baeb6b20866f6845
DaniyarKulmanov <DanRubyDeveloper@gmail.com>
2021-11-16 21:26:09 +0600

    admin gists_controller fix

--- app/controllers/admin/gists_controller.rb
+++ app/controllers/admin/gists_controller.rb
@@ -2,22 +2,16 @@
 
 class Admin::GistsController < Admin::BaseController
   before_action :authenticate_user!
-  before_action :set_gist, only: :destroy
 
   def index
     @gists = Gist.all
   end
 
   def destroy
+    @gist = Gist.find(params[:id])
     @gist.destroy
     GistQuestionService.destroy(@gist.guid)
 
     redirect_to admin_gists_path
-  end
-
-private
-
-  def set_gist
-    @gist = Gist.find(params[:id])
   end
 end
