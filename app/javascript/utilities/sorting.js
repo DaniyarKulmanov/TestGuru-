7d7c7a85ea3eb20d98061a9fca29010c9a6d0dae
DaniyarKulmanov <DanRubyDeveloper@gmail.com>
2021-11-29 09:45:16 +0600

    sorting.js

--- app/javascript/utilities/sorting.js
+++ app/javascript/utilities/sorting.js
@@ -1,7 +1,48 @@
 document.addEventListener('turbolinks:load', function() {
-  var control = document.querySelector('.sort-by-title')
+  let control = document.querySelector('.sort-by-title')
 
-  control.addEventListener('click',  function() {
-    alert('Click')
-  })
+  control.addEventListener('click', sortRowsByTitle)
 })
+
+
+function sortRowsByTitle() {
+  let table = document.querySelector('table')
+
+  // Nodelist
+  // https://developer.mozilla.org/en-US/docs/Web/API/NodeList
+  let rows = table.querySelectorAll('tr')
+  let sortedRows = []
+
+  // select all table rows expect first because it is a header
+  for (let n = 1; n < rows.length; n++) {
+    sortedRows.push(rows[n])
+  }
+
+  sortedRows.sort(compareRows)
+
+  let sortedTable = document.createElement('table')
+  let thead = document.createElement('thead')
+  let tbody = document.createElement('tbody')
+
+  thead.appendChild(rows[0])
+  sortedTable.classList.add('table')
+  sortedTable.appendChild(thead)
+
+  for (let n = 0; n < sortedRows.length; n++) {
+    let tbody = document.createElement('tbody')
+    tbody.appendChild(sortedRows[n]) // need to insert tbody? incorrect html
+    sortedTable.appendChild(tbody) // need to insert tbody? incorrect html
+  }
+  console.log(table)
+  console.log(sortedTable)
+  table.parentNode.replaceChild(sortedTable, table)
+}
+
+function compareRows(row1, row2) {
+  let testTitle1 = row1.querySelector('td').textContent
+  let testTitle2 = row2.querySelector('td').textContent
+
+  if (testTitle1 < testTitle2) { return -1 }
+  if (testTitle1 > testTitle2) { return 1 }
+  return 0
+}
