76422e86bfe8e8016a9131c0f91bd51a14311d0f
DaniyarKulmanov <DanRubyDeveloper@gmail.com>
2021-12-02 19:40:52 +0600

    password matching complete

--- app/javascript/utilities/password_match.js
+++ app/javascript/utilities/password_match.js
@@ -1,19 +1,17 @@
 // TODO: only on registration views
 document.addEventListener('turbolinks:load', function() {
-  let control = []
-  control.push(document.querySelector('#user_password'))
-  control.push(document.querySelector('#user_password_confirmation'))
+  let userPasswordConfirm = document.querySelector('#user_password_confirmation')
 
-  console.log(`Hello! from addEventListener ${control[0]}`)
-
-  for (i = 0; i < control.length; i++) {
-    if (control[i]) {
-      control[i].addEventListener('input', passwordMathCheck)
-    }
-  }
+  if (userPasswordConfirm) { userPasswordConfirm.addEventListener('input', passwordMathCheck) }
 
 })
 
 function passwordMathCheck() {
-  console.log('Hello! from passwordMathCheck')
+
+  if (document.getElementById('user_password').value ==
+      document.getElementById('user_password_confirmation').value) {
+      console.log('match')
+    } else {
+      console.log('not match')
+    }
 }
