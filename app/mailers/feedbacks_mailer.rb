766f428dd7c4e7713b3635a5a67ed9343364195e
DaniyarKulmanov <DanRubyDeveloper@gmail.com>
2022-01-24 12:02:02 +0600

    feedback mailer

--- app/mailers/feedbacks_mailer.rb
+++ app/mailers/feedbacks_mailer.rb
@@ -1,2 +1,12 @@
+# frozen_string_literal: true
+
 class FeedbacksMailer < ApplicationMailer
+  default to: -> { Admin.pluck(:email) }
+
+  def feedback_created_notification(user, body)
+    @user = user
+    @body = body
+
+    mail(subject: t('.feedback', user: @user.email))
+  end
 end
--- test/mailers/previews/feedbacks_mailer_preview.rb
+++ test/mailers/previews/feedbacks_mailer_preview.rb
@@ -1,4 +1,9 @@
 # Preview all emails at http://localhost:3000/rails/mailers/feedbacks_mailer
 class FeedbacksMailerPreview < ActionMailer::Preview
+  def feedback_created_notification
+    @user = User.first
+    @body = 'Test text'
 
+    FeedbacksMailer.feedback_created_notification(@user, @body)
+  end
 end
