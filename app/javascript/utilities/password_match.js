2c09d0e4ef5f1e14e9730fb5a1dac9ff1fac3da4
DaniyarKulmanov <DanRubyDeveloper@gmail.com>
2022-01-25 19:36:45 +0600

    password check js fix to check both fields

--- app/javascript/utilities/password_match.js
+++ app/javascript/utilities/password_match.js
@@ -1,7 +1,9 @@
 document.addEventListener('turbolinks:load', function() {
   let userPasswordConfirm = document.querySelector('#user_password_confirmation')
+  let userPassword = document.querySelector('#user_password')
 
   if (userPasswordConfirm) { userPasswordConfirm.addEventListener('input', passwordMathCheck) }
+  if (userPassword) { userPassword.addEventListener('input', passwordMathCheck) }
 
 })
 
@@ -13,10 +15,10 @@ function passwordMathCheck() {
   let circleSlash = document.querySelectorAll('.octicon-circle-slash')
   let checkCircle = document.querySelectorAll('.octicon-check-circle')
 
-  if (password.value == passwordConfirm.value) {
+  if ((password.value === passwordConfirm.value) && (passwordConfirm.textLength !== 0 && password.textLength !== 0)) {
     circleSlash.forEach(icon => { icon.classList.add('hide') })
     checkCircle.forEach(icon => { icon.classList.remove('hide') })
-  } else if (passwordConfirm.textLength == 0) {
+  } else if (passwordConfirm.textLength === 0 || password.textLength === 0) {
     circleSlash.forEach(icon => { icon.classList.add('hide') })
     checkCircle.forEach(icon => { icon.classList.add('hide') })
   } else {
