23d1a89cefac2fe36ed13bdd80ac37dba20ce6d8
DaniyarKulmanov <DanRubyDeveloper@gmail.com>
2022-02-02 10:00:13 +0600

    localization: email already taken, password to short

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -1,17 +1,14 @@
 class FeedbacksController < ApplicationController
   before_action :authenticate_user!
 
-  def new
-
-  end
+  def new; end
 
   def create
-    @user = current_user
     @body = params[:feedback][:body]
-
+    # TODO use FeedbackForm, @feedback_form.save?
     if @body.present?
       flash[:alert] = t('.success')
-      FeedbacksMailer.created_notification(@user, @body).deliver_now
+      FeedbacksMailer.created_notification(current_user, @body).deliver_now
       redirect_to root_path
     else
       flash[:alert] = t('.failure')
