4190529bd0bdd68489dae681fc6a968e05d8248d
DaniyarKulmanov <DanRubyDeveloper@gmail.com>
2022-02-02 11:57:15 +0600

    rubocop exclude schema.rb & migration files, use on seeds files

--- db/seeds/001_users.rb
+++ db/seeds/001_users.rb
@@ -4,11 +4,11 @@ PASSWORD = ENV.fetch('INITIAL_PASSWORD')
 
 5.times do
   User.create(email: Faker::Internet.email,
-                         first_name: Faker::Name.first_name,
-                         last_name: Faker::Name.last_name,
-                         password: PASSWORD,
-                         password_confirmation: PASSWORD,
-                         confirmed_at: Time.now)
+              first_name: Faker::Name.first_name,
+              last_name: Faker::Name.last_name,
+              password: PASSWORD,
+              password_confirmation: PASSWORD,
+              confirmed_at: Time.now)
 end
 
 Admin.create(email: Faker::Internet.email,
