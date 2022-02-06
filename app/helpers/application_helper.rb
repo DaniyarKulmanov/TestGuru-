95d2adfc8f953e742528e01e7520650efd56cbcc
DaniyarKulmanov <DanRubyDeveloper@gmail.com>
2022-02-06 13:37:15 +0600

    en-ru swticher

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,12 @@ module ApplicationHelper
     title = repo.nil? ? "Автор #{author}" : "Проект #{repo}"
     link_to title, "https://github.com/#{author}/#{repo}", target: '_blank'
   end
+
+  def change_locale
+    if I18n.locale == :ru
+      link_to 'EN', url_for(lang: :en)
+    else
+      link_to 'RU', url_for(lang: :ru)
+    end
+  end
 end
