c35b315260f1fa1bb755f6129bd36b998ad91d08
DaniyarKulmanov <DanRubyDeveloper@gmail.com>
2022-03-03 13:46:58 +0600

    parameter validations, based on enum

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -7,4 +7,8 @@ class Badge < ApplicationRecord
   has_many :users, through: :earnings
 
   validates :name, :filename, presence: true
+  validates :parameter, inclusion: { in: Category.pluck(:title) }, if: :criteria_category?
+  validates :parameter, inclusion: { in: ('1'..'99 ')}, if: :criteria_attempts?
+  validates :parameter, inclusion: { in: Test.pluck(:title) }, if: :criteria_named?
+  validates :parameter, inclusion: { in: Test.all.map{|test| test.level.to_s} }, if: :criteria_level?
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_01_140203) do
+ActiveRecord::Schema.define(version: 2022_03_03_020456) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2022_03_01_140203) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "criteria", default: 0
+    t.string "parameter"
   end
 
   create_table "categories", force: :cascade do |t|
