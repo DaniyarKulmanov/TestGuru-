8a1c79071c60ba120b8e33f31e1f359eec4c16ac
DaniyarKulmanov <DanRubyDeveloper@gmail.com>
2022-03-04 08:55:16 +0600

    dinamyc parameter select options

---
+++ app/javascript/utilities/criteria_parameter.js
@@ -0,0 +1,45 @@
+document.addEventListener('turbolinks:load', function() {
+    let control = document.getElementById('badge_criteria')
+
+    if (control) { control.addEventListener('click', fillCriteriaParameters) }
+})
+
+function fillCriteriaParameters(){
+    let criteriaSelected = document.getElementById('badge_criteria').value
+    let parameterValues = document.getElementById('badge_parameter')
+
+    let categoryTitles = document.getElementById('category_titles').value.split(' ')
+    let attemptValues = document.getElementById('attempt_values').value.split(' ')
+    let testTitles = document.getElementById('test_titles').value.split(' ')
+    let testLevels = document.getElementById('test_levels').value.split(' ')
+
+    switch (criteriaSelected) {
+        case 'category':
+            newOptionsForParameters(categoryTitles, parameterValues)
+            break
+        case 'attempts':
+            newOptionsForParameters(attemptValues, parameterValues)
+            break
+        case 'named':
+            newOptionsForParameters(testTitles, parameterValues)
+            break
+        case 'level':
+            newOptionsForParameters(testLevels, parameterValues)
+            break
+    }
+}
+
+function newOptionsForParameters( newOptions, selectOptionParameters ) {
+    removeOptions(selectOptionParameters)
+
+    for(let i = 0; i < newOptions.length; i++){
+        selectOptionParameters.options.add(new Option(newOptions[i], newOptions[i]));
+    }
+}
+
+function removeOptions(selectElement) {
+    let i, L = selectElement.options.length - 1;
+    for(i = L; i >= 0; i--) {
+        selectElement.remove(i);
+    }
+}
