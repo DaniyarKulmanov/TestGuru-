07518765d76c76c16e67359ffe36e6a4ccdcecab
DaniyarKulmanov <DanRubyDeveloper@gmail.com>
2022-03-14 08:59:34 +0600

    badge views translation

--- app/helpers/badges_helper.rb
+++ app/helpers/badges_helper.rb
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
+
 module BadgesHelper
+  def human_enum_name(enum_name, enum_value)
+    I18n.t("activerecord.attributes.badge.#{enum_name.to_s.pluralize}.#{enum_value}")
+  end
 end
