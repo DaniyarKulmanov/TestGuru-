d0cf22cc8cb0b099672528a12b5ed705d20aaa85
DaniyarKulmanov <DanRubyDeveloper@gmail.com>
2022-04-20 09:19:09 +0600

    refactor badge parameter name in views index/show

--- app/helpers/badges_helper.rb
+++ app/helpers/badges_helper.rb
@@ -4,4 +4,22 @@ module BadgesHelper
   def human_enum_name(enum_name, enum_value)
     I18n.t("activerecord.attributes.badge.#{enum_name.to_s.pluralize}.#{enum_value}")
   end
+
+  def parameter_name(badge)
+    category_or_named?(badge) ? read_parameter_name(badge) : badge.parameter
+  end
+
+  private
+
+  def read_parameter_name(badge)
+    if badge.criteria_category?
+      Category.find(badge.parameter).title
+    else
+      Test.find(badge.parameter).title
+    end
+  end
+
+  def category_or_named?(badge)
+    badge.criteria_category? || badge.criteria_named?
+  end
 end
